111e71a2df74f5701ed36cbdc0535e13e14e911a
Geoffrey Challen <challen@illinois.edu>
2018-01-01 19:10:08 -0500

    Trying to remove Grunt.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const appRootPath = require('app-root-path')
 const path = require('path')
+const webpackConfiguration = require('./webpack.config.js')
 
 const metalsmith = require('metalsmith')
 const ignore = require('metalsmith-ignore')
@@ -35,110 +36,91 @@ const tmp = require('tmp')
 const fs = require('fs')
 const removeEmptyDirectories = require('remove-empty-directories')
 const rsync = require('rsync')
+const _ = require('lodash')
 
-const _ = require('underscore');
+const syllabusPattern = 'syllabus/**/*.adoc'
 
 const defaults = {
   verbose: false,
   source: 'src',
   destination: 'build'
-};
-
-var syllabusPattern = 'syllabus/**/*.adoc';
-
-function build(config, done) {
-  config = _.extend(_.clone(defaults), config || {});
-
-  const temporaryDestination = tmp.dirSync({ mode: 0775 }).name;
-  var webpackConfiguration = require('./webpack.config.js');
-  webpackConfiguration.output.path = temporaryDestination;
-
-  var quiet = (config.quiet == true);
-
-  metalsmith(__dirname)
-    .source(config.source)
-    .destination(temporaryDestination)
-    .use(ignore([
-      'fonts/*',
-      'css/*',
-      '**/*.swp',
-      '**/*.swo'
-    ]))
-    .use(buildDate())
-    .use(drafts())
-    .use(metadata({
-      course: 'course.yaml',
-      dates: 'schedule/dates.yaml'
-    }))
-    .use(registerPartials())
-    .use(course())
-    .use(people())
-    .use(inPlace({
-      pattern: '**/*.adoc.hbs',
-    }))
-    .use(empty())
-    .use(asciidoc())
-    .use(markdown())
-    .use(footnotes())
-    .use(webpack(webpackConfiguration))
-    .use(inPlace({
-      pattern: '**/*.html.hbs',
-    }))
-    .use(sections())
-    .use(permalinks({ relative: false }))
-    .use(hacks.preLayout())
-    .use(layouts({
-      engine: 'handlebars'
-    }))
-    .use(fixPath())
-    .use(active())
-    .use(external())
-    .use(iframes())
-    .use(hacks.postLayout())
-    .use(highlight())
-    .use(internalize())
-    .use(msif((config.check),
-      spellcheck({ dicFile: 'dicts/en_US.dic',
-                   affFile: 'dicts/en_US.aff',
-                   exceptionFile: 'dicts/spelling_exceptions.yaml',
-                   checkedPart: "#content",
-                   failErrors: false,
-                   verbose: !quiet})))
-    .use(msif(config.check,
-      formatcheck({ verbose: !quiet , failWithoutNetwork: false })))
-    .use(msif(config.check,
-      linkcheck({ verbose: !quiet , failWithoutNetwork: false })))
-    .build(function(err) {
-      if (err) {
-        done(err);
-      } else {
-        removeEmptyDirectories(temporaryDestination);
-        var copyToRealDestination = new rsync()
-          .flags('rlgoDc')
-          .delete()
-          .source(temporaryDestination + "/")
-          .destination(config.destination);
-        copyToRealDestination.execute(function (err) {
-          done(err);
-        });
-      }
-    });
 }
+const config = _.extend(_.clone(defaults), require('minimist')(process.argv.slice(2)) || {})
 
-if (require.main === module) {
-  var config = require('minimist')(process.argv.slice(2));
-  build(config, function(err) {
+const temporaryDestination = tmp.dirSync({ mode: 0775 }).name
+webpackConfiguration.output.path = temporaryDestination
+
+const quiet = (config.quiet == true)
+
+metalsmith(__dirname)
+  .source(config.source)
+  .destination(temporaryDestination)
+  .use(ignore([
+    'fonts/*',
+    'css/*',
+    '**/*.swp',
+    '**/*.swo'
+  ]))
+  .use(buildDate())
+  .use(drafts())
+  .use(metadata({
+    course: 'course.yaml',
+    dates: 'schedule/dates.yaml'
+  }))
+  .use(registerPartials())
+  .use(course())
+  .use(people())
+  .use(inPlace({
+    pattern: '**/*.adoc.hbs',
+  }))
+  .use(empty())
+  .use(asciidoc())
+  .use(markdown())
+  .use(footnotes())
+  .use(webpack(webpackConfiguration))
+  .use(inPlace({
+    pattern: '**/*.html.hbs',
+  }))
+  .use(sections())
+  .use(permalinks({ relative: false }))
+  .use(hacks.preLayout())
+  .use(layouts({
+    engine: 'handlebars'
+  }))
+  .use(fixPath())
+  .use(active())
+  .use(external())
+  .use(iframes())
+  .use(hacks.postLayout())
+  .use(highlight())
+  .use(internalize())
+  .use(msif((config.check),
+    spellcheck({ dicFile: 'dicts/en_US.dic',
+                 affFile: 'dicts/en_US.aff',
+                 exceptionFile: 'dicts/spelling_exceptions.yaml',
+                 checkedPart: "#content",
+                 failErrors: false,
+                 verbose: !quiet})))
+  .use(msif(config.check,
+    formatcheck({ verbose: !quiet , failWithoutNetwork: false })))
+  .use(msif(config.check,
+    linkcheck({ verbose: !quiet , failWithoutNetwork: false })))
+  .build(err => {
     if (err) {
-      throw err;
+      throw(err)
+    } else {
+      removeEmptyDirectories(temporaryDestination)
+      var copyToRealDestination = new rsync()
+        .flags('rlgoDc')
+        .delete()
+        .source(temporaryDestination + "/")
+        .destination(config.destination)
+      copyToRealDestination.execute(err => {
+        if (err) {
+          throw(err)
+        }
+      })
     }
-  });
-} else {
-  module.exports = function (grunt) {
-    grunt.registerTask('build', 'Build the site.', function(config) {
-      var done = this.async();
-      build(this.options(), done);
-    });
-  }
-}
+  })
 
 // vim: ts=2:sw=2:et:ft=javascript
