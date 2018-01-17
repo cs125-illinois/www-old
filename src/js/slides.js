5c9e83de9931cc368f7a974abc2584c07fe3402f
Geoffrey Challen <challen@illinois.edu>
2018-01-17 02:36:37 -0600

    Code examples working.

--- src/js/slides.js
+++ src/js/slides.js
@@ -7,10 +7,16 @@ require("codemirror/addon/edit/closebrackets")
 require("codemirror/lib/codemirror.css")
 
 const janini = () => {
-  $("textarea.janini").each((i, element) => {
-    let newCodeMirror = CodeMirror.fromTextArea($(element).get(0))
-  })
   return (deck) => {
+    let janinis = {}
+    _.each(deck.slides, (element, i) => {
+      $(element).find("textarea.janini").each((unused, element) => {
+        let newCodeMirror = CodeMirror.fromTextArea($(element).get(0))
+        janinis[i] = newCodeMirror
+      })
+    })
+    let active
+
     let firstSlide = deck.slides[0]
     let slideHeight = firstSlide.offsetHeight
     let slideWidth = firstSlide.offsetWidth
@@ -35,7 +41,44 @@ const janini = () => {
     noZoomResize()
 
     deck.on('activate', e => {
-      console.log(e.slide)
+      active = e
+    })
+
+    $(window).keypress(function (event) {
+      if (!(event.which === 115 && event.ctrlKey) && !(event.which === 19)) {
+        return true
+      } else if (!(janinis[active.index])) {
+        return true
+      }
+      event.preventDefault()
+      let source = janinis[active.index]
+      let output = $(active.slide).find('.output').first()
+
+      let toRun = source.getValue()
+      if (toRun.trim() === "") {
+        $(output).text(`> Hit Control-S to run your Java code...`)
+        return
+      }
+
+      $.post("https://cs125.cs.illinois.edu/janini/", JSON.stringify({
+        source: source.getValue()
+      })).done(result => {
+        console.log(JSON.stringify(result, null, 2))
+        if (result.completed) {
+          $(output).text(result.output)
+        } else if (result.timeout) {
+          $(output).html(`<span class="text-danger">Timeout</span>`)
+        } else if (!result.compiled) {
+          $(output).html(`<span class="text-danger">Compiler error:\n${ result.compileError }</span>`)
+        } else if (!result.ran) {
+          $(output).html(`<span class="text-danger">Runtime error:\n${ result.runtimeError }</span>`)
+        }
+      }).fail((xhr, status, error) => {
+        console.error("Request failed")
+        console.error(JSON.stringify(xhr, null, 2))
+        console.error(JSON.stringify(status, null, 2))
+        console.error(JSON.stringify(error, null, 2))
+      })
     })
   }
 }
