6111a0caec6ce207e52c479bd6ea95933dcd4523
Geoffrey Challen <challen@illinois.edu>
2018-01-21 20:37:40 -0600

    Improve loading.

--- src/js/slides.js
+++ src/js/slides.js
@@ -1,5 +1,6 @@
 const clientID = '948918026196-p399ooibc7pr0ci7ida63jb5a6n4vsik.apps.googleusercontent.com'
 
+require('jquery')
 const bespoke = require('./slides/bespoke.js')
 const classes = require('bespoke-classes')
 const nav = require('bespoke-nav')
@@ -31,3 +32,20 @@ bespoke.from({
   janini()
 ])
 
+$(() => {
+  $('div.lazyiframe').each(function() {
+    var iframe = $("<iframe/>")
+    $.each(JSON.parse(decodeURI($(this).data('attribs'))), (name, value) => {
+      $(iframe).attr(name, value);
+    })
+    $(this).replaceWith(iframe)
+  })
+  $('[data-toggle="popover"]').popover()
+  $('body').on('click', e => {
+    $('[data-toggle="popover"]').each(function() {
+      if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+        $(this).popover('hide');
+      }
+    })
+  })
+})
