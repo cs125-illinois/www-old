bf1e5a5b4b7448268e09b115200ccf63de63d53c
Geoffrey Challen <challen@illinois.edu>
2018-09-05 11:36:49 -0500

    Template helper bug fix.

--- layouts/helpers/and_list.js
+++ layouts/helpers/and_list.js
@@ -4,7 +4,9 @@ module.exports = (items, options) => {
   let list = items.map(function (e) {
     return "<span style='white-space: nowrap;'>" + options.fn(e) + "</span>";
   })
-  if (list.length == 1) {
+  if (list.length === 0) {
+    list = ""
+  } else if (list.length == 1) {
     list = list[0]
   } else if (list.length == 2) {
     list = list.join(" and ")
